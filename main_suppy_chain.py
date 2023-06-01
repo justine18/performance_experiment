627bb8a4523be54b645975b6c43e3c271ba31b8c
Justine Broihan <jbroihan@gams.com>
2023-06-01 15:55:24 +0200

    adjust intuitive pyomo

--- main_suppy_chain.py
+++ main_suppy_chain.py
@@ -47,32 +47,34 @@ def run_experiment(
         # create variable data and convert to tuples
         (
             I,
-            ij_tuple,
             ik_tuple,
-            jk_tuple,
-            kl_tuple,
-            lm_tuple,
+            il_tuple,
+            im_tuple,
+            ijk_tuple,
+            ikl_tuple,
+            ilm_tuple,
             d_dict,
         ) = data.create_variable_data(n=n, J=J, K=K, L=L, M=M)
 
         # save data to json for JuMP
-        save_to_json(ij_tuple, "IJ", f"_{n}", "supply_chain")
         save_to_json(ik_tuple, "IK", f"_{n}", "supply_chain")
-        save_to_json(kl_tuple, "KL", f"_{n}", "supply_chain")
-        save_to_json(lm_tuple, "LM", f"_{n}", "supply_chain")
+        save_to_json(il_tuple, "IL", f"_{n}", "supply_chain")
+        save_to_json(im_tuple, "IM", f"_{n}", "supply_chain")
+        save_to_json(ijk_tuple, "IJK", f"_{n}", "supply_chain")
+        save_to_json(ikl_tuple, "IKL", f"_{n}", "supply_chain")
+        save_to_json(ilm_tuple, "ILM", f"_{n}", "supply_chain")
         save_to_json_d(d_dict, "D", f"_{n}", "supply_chain")
 
         # Intuitive Pyomo
         if below_time_limit(df_intuitive_pyomo, time_limit):
             rr = run_intuitive_pyomo(
                 I=I,
-                L=L,
-                M=M,
-                IJ=ij_tuple,
-                JK=jk_tuple,
                 IK=ik_tuple,
-                KL=kl_tuple,
-                LM=lm_tuple,
+                IL=il_tuple,
+                IM=im_tuple,
+                IJK=ijk_tuple,
+                IKL=ikl_tuple,
+                ILM=ilm_tuple,
                 D=d_dict,
                 solve=solve,
                 repeats=repeats,
@@ -82,23 +84,23 @@ def run_experiment(
             print_log_message(language="Intuitive Pyomo", n=n, df=df_intuitive_pyomo)
 
         # Pyomo
-        if below_time_limit(df_pyomo, time_limit):
-            rr = run_pyomo(
-                I=I,
-                L=L,
-                M=M,
-                IJ=ij_tuple,
-                JK=jk_tuple,
-                IK=ik_tuple,
-                KL=kl_tuple,
-                LM=lm_tuple,
-                D=d_dict,
-                solve=solve,
-                repeats=repeats,
-                number=number,
-            )
-            df_pyomo = process_results(rr, df_pyomo)
-            print_log_message(language="Pyomo", n=n, df=df_pyomo)
+        # if below_time_limit(df_pyomo, time_limit):
+        #     rr = run_pyomo(
+        #         I=I,
+        #         L=L,
+        #         M=M,
+        #         IJ=ij_tuple,
+        #         JK=jk_tuple,
+        #         IK=ik_tuple,
+        #         KL=kl_tuple,
+        #         LM=lm_tuple,
+        #         D=d_dict,
+        #         solve=solve,
+        #         repeats=repeats,
+        #         number=number,
+        #     )
+        #     df_pyomo = process_results(rr, df_pyomo)
+        #     print_log_message(language="Pyomo", n=n, df=df_pyomo)
 
     # JuMP
     df_jump, df_intuitive_jump = run_julia(solve, repeats, number, time_limit)
--- supply_chain/run_pyomo.py
+++ supply_chain/run_pyomo.py
@@ -11,22 +11,20 @@ logging.getLogger("pyomo.core").setLevel(logging.ERROR)
 
 
 ########## Intuitive Pyomo ##########
-def run_intuitive_pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve, repeats, number):
+def run_intuitive_pyomo(I, IK, IL, IM, IJK, IKL, ILM, D, solve, repeats, number):
     setup = {
-        "I": I,
-        "L": L,
-        "M": M,
-        "IJ": IJ,
-        "JK": JK,
         "IK": IK,
-        "KL": KL,
-        "LM": LM,
+        "IL": IL,
+        "IM": IM,
+        "IJK": IJK,
+        "IKL": IKL,
+        "ILM": ILM,
         "D": D,
         "solve": solve,
         "model_function": intuitive_pyomo,
     }
     r = timeit.repeat(
-        "model_function(I, L, M, IJ, JK, IK, KL, LM, D, solve)",
+        "model_function(IK, IL, IM, IJK, IKL, ILM, D, solve)",
         repeat=repeats,
         number=number,
         globals=setup,
@@ -44,46 +42,30 @@ def run_intuitive_pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve, repeats, number):
     return result
 
 
-def intuitive_pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve):
+def intuitive_pyomo(IK, IL, IM, IJK, IKL, ILM, D, solve):
     model = pyo.ConcreteModel()
 
-    model.I = pyo.Set(initialize=I)
-    model.L = pyo.Set(initialize=L)
-    model.M = pyo.Set(initialize=M)
-    model.IJ = pyo.Set(initialize=IJ)
-    model.JK = pyo.Set(initialize=JK)
     model.IK = pyo.Set(initialize=IK)
-    model.KL = pyo.Set(initialize=KL)
-    model.LM = pyo.Set(initialize=LM)
+    model.IL = pyo.Set(initialize=IL)
+    model.IM = pyo.Set(initialize=IM)
+    model.IJK = pyo.Set(initialize=IJK)
+    model.IKL = pyo.Set(initialize=IKL)
+    model.ILM = pyo.Set(initialize=ILM)
 
     model.f = pyo.Param(default=1)
-    model.d = pyo.Param(model.I, model.M, initialize=D)
+    model.d = pyo.Param(model.IM, initialize=D)
 
-    model.x = pyo.Var(
-        [
-            (i, j, k)
-            for (i, j) in model.IJ
-            for (jj, k) in model.JK
-            if jj == j
-            for (ii, kk) in model.IK
-            if (ii == i) and (kk == k)
-        ],
-        domain=pyo.NonNegativeReals,
-    )
+    model.x = pyo.Var(model.IJK, domain=pyo.NonNegativeReals)
 
-    model.y = pyo.Var(
-        [(i, k, l) for i in model.I for (k, l) in model.KL], domain=pyo.NonNegativeReals
-    )
+    model.y = pyo.Var(model.IKL, domain=pyo.NonNegativeReals)
 
-    model.z = pyo.Var(
-        [(i, l, m) for i in model.I for (l, m) in model.LM], domain=pyo.NonNegativeReals
-    )
+    model.z = pyo.Var(model.ILM, domain=pyo.NonNegativeReals)
 
     model.OBJ = pyo.Objective(expr=model.f)
 
     model.production = pyo.Constraint(model.IK, rule=intuitive_production_rule)
-    model.transport = pyo.Constraint(model.I, model.L, rule=intuitive_transport_rule)
-    model.demand = pyo.Constraint(model.I, model.M, rule=intuitive_demand_rule)
+    model.transport = pyo.Constraint(model.IL, rule=intuitive_transport_rule)
+    model.demand = pyo.Constraint(model.IM, rule=intuitive_demand_rule)
 
     # model.write("int.lp")
 
@@ -93,33 +75,22 @@ def intuitive_pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve):
 
 
 def intuitive_production_rule(model, i, k):
-    lhs = [
-        model.x[i, j, k]
-        for (ii, j) in model.IJ
-        if ii == i
-        for (jj, kk) in model.JK
-        if (jj == j) and (kk == k)
-    ]
-    rhs = [model.y[i, k, l] for (kk, l) in model.KL if kk == k]
-
-    if lhs or rhs:
-        return sum(lhs) >= sum(rhs)
-    else:
-        return pyo.Constraint.Skip
+    return sum(
+        model.x[i, j, k] for (ii, j, kk) in model.IJK if ii == i and kk == k
+    ) >= sum(model.y[i, k, l] for (ii, kk, l) in model.IKL if ii == i and kk == k)
 
 
 def intuitive_transport_rule(model, i, l):
-    lhs = [model.y[i, k, l] for (k, ll) in model.KL if ll == l]
-    rhs = [model.z[i, l, m] for (lll, m) in model.LM if lll == l]
-
-    if lhs or rhs:
-        return sum(lhs) >= sum(rhs)
-    else:
-        return pyo.Constraint.Skip
+    return sum(
+        model.y[i, k, l] for (ii, k, ll) in model.IKL if ii == i and ll == l
+    ) >= sum(model.z[i, l, m] for (ii, ll, m) in model.ILM if ii == i and ll == l)
 
 
 def intuitive_demand_rule(model, i, m):
-    return sum(model.z[i, l, m] for (l, mm) in model.LM if mm == m) >= model.d[i, m]
+    return (
+        sum(model.z[i, l, m] for (ii, l, mm) in model.ILM if ii == i and mm == m)
+        >= model.d[i, m]
+    )
 
 
 ########## Pyomo ##########
