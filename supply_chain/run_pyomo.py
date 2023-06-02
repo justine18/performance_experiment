88c2559e5d62b8239d25ea9241158124919161af
Justine Broihan <jbroihan@gams.com>
2023-06-02 14:56:21 +0200

    adjust fast implementations

--- supply_chain/run_pyomo.py
+++ supply_chain/run_pyomo.py
@@ -56,9 +56,7 @@ def intuitive_pyomo(IK, IL, IM, IJK, IKL, ILM, D, solve):
     model.d = pyo.Param(model.IM, initialize=D)
 
     model.x = pyo.Var(model.IJK, domain=pyo.NonNegativeReals)
-
     model.y = pyo.Var(model.IKL, domain=pyo.NonNegativeReals)
-
     model.z = pyo.Var(model.ILM, domain=pyo.NonNegativeReals)
 
     model.OBJ = pyo.Objective(expr=model.f)
@@ -94,22 +92,62 @@ def intuitive_demand_rule(model, i, m):
 
 
 ########## Pyomo ##########
-def run_pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve, repeats, number):
+def run_pyomo(I, IK, IL, IM, IJK, IKL, ILM, D, solve, repeats, number):
+    IK_IJK = {(i, k): [] for (i, k) in IK}
+    IK_IKL = {(i, k): [] for (i, k) in IK}
+    IL_IKL = {(i, l): [] for (i, l) in IL}
+    IL_ILM = {(i, l): [] for (i, l) in IL}
+    IM_ILM = {(i, m): [] for (i, m) in IM}
+
+    IK_IJK.update(
+        {
+            (i, k): list(group)
+            for (i, k), group in itertools.groupby(sorted(IJK), itemgetter(0, 2))
+        }
+    )
+    IK_IKL.update(
+        {
+            (i, k): list(group)
+            for (i, k), group in itertools.groupby(sorted(IKL), itemgetter(0, 1))
+        }
+    )
+    IL_IKL.update(
+        {
+            (i, l): list(group)
+            for (i, l), group in itertools.groupby(sorted(IKL), itemgetter(0, 2))
+        }
+    )
+    IL_ILM.update(
+        {
+            (i, l): list(group)
+            for (i, l), group in itertools.groupby(sorted(ILM), itemgetter(0, 1))
+        }
+    )
+    IM_ILM.update(
+        {
+            (i, m): list(group)
+            for (i, m), group in itertools.groupby(sorted(ILM), itemgetter(0, 2))
+        }
+    )
+
     setup = {
-        "I": I,
-        "L": L,
-        "M": M,
-        "IJ": IJ,
-        "JK": JK,
         "IK": IK,
-        "KL": KL,
-        "LM": LM,
+        "IL": IL,
+        "IM": IM,
+        "IJK": IJK,
+        "IKL": IKL,
+        "ILM": ILM,
+        "IK_IJK": IK_IJK,
+        "IK_IKL": IK_IKL,
+        "IL_IKL": IL_IKL,
+        "IL_ILM": IL_ILM,
+        "IM_ILM": IM_ILM,
         "D": D,
         "solve": solve,
         "model_function": pyomo,
     }
     r = timeit.repeat(
-        "model_function(I, L, M, IJ, JK, IK, KL, LM, D, solve)",
+        "model_function(IK, IL, IM, IJK, IKL, ILM, IK_IJK, IK_IKL, IL_IKL, IL_ILM, IM_ILM,D, solve)",
         repeat=repeats,
         number=number,
         globals=setup,
@@ -127,82 +165,34 @@ def run_pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve, repeats, number):
     return result
 
 
-def pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve):
+def pyomo(IK, IL, IM, IJK, IKL, ILM, IK_IJK, IK_IKL, IL_IKL, IL_ILM, IM_ILM, D, solve):
     model = pyo.ConcreteModel()
 
-    model.I = pyo.Set(initialize=I)
-    model.L = pyo.Set(initialize=L)
-    model.M = pyo.Set(initialize=M)
-    model.IJ = pyo.Set(initialize=IJ)
-    model.JK = pyo.Set(initialize=JK)
     model.IK = pyo.Set(initialize=IK)
-    model.KL = pyo.Set(initialize=KL)
-    model.LM = pyo.Set(initialize=LM)
+    model.IL = pyo.Set(initialize=IL)
+    model.IM = pyo.Set(initialize=IM)
+    model.IJK = pyo.Set(initialize=IJK)
+    model.IKL = pyo.Set(initialize=IKL)
+    model.ILM = pyo.Set(initialize=ILM)
 
-    # some data wrangling
-    IJ_dict = defaultdict(set)
-    for i, j in IJ:
-        IJ_dict[i].add(j)
-    KJ_dict = defaultdict(set)
-    for j, k in JK:
-        KJ_dict[k].add(j)
-    # make a dictionary of (i, k) : {(i, j, k) tuples}
-    IK_IJK = {
-        (i, k): {(i, j, k) for j in IJ_dict.get(i, set()) & KJ_dict.get(k, set())}
-        for (i, k) in IK
-    }
-
-    L_M = {l: [] for l in model.L}
-    L_M.update(
-        {
-            l: list(map(itemgetter(1), lm))
-            for l, lm in itertools.groupby(sorted(model.LM), itemgetter(0))
-        }
-    )
-    M_L = {m: [] for m in model.M}
-    M_L.update(
-        {
-            m: list(map(itemgetter(0), lm))
-            for m, lm in itertools.groupby(
-                sorted(model.LM, key=itemgetter(1)), itemgetter(1)
-            )
-        }
-    )
-    L_K = {l: [] for l in model.L}
-    L_K.update(
-        {
-            l: list(map(itemgetter(0), kl))
-            for l, kl in itertools.groupby(
-                sorted(model.KL, key=itemgetter(1)), itemgetter(1)
-            )
-        }
-    )
-
-    model.L_M = pyo.Set(model.L, initialize=L_M)
-    model.M_L = pyo.Set(model.M, initialize=M_L)
-    model.L_K = pyo.Set(model.L, initialize=L_K)
     model.IK_IJK = pyo.Set(IK_IJK.keys(), initialize=IK_IJK)
+    model.IK_IKL = pyo.Set(IK_IKL.keys(), initialize=IK_IKL)
+    model.IL_IKL = pyo.Set(IL_IKL.keys(), initialize=IL_IKL)
+    model.IL_ILM = pyo.Set(IL_ILM.keys(), initialize=IL_ILM)
+    model.IM_ILM = pyo.Set(IM_ILM.keys(), initialize=IM_ILM)
 
     model.f = pyo.Param(default=1)
-    model.d = pyo.Param(model.I, model.M, initialize=D)
+    model.d = pyo.Param(model.IM, initialize=D)
 
-    x_list = list(itertools.chain(*IK_IJK.values()))
-
-    model.x = pyo.Var(x_list, domain=pyo.NonNegativeReals)
-
-    model.y = pyo.Var(
-        [(i, k, l) for i in model.I for (k, l) in model.KL], domain=pyo.NonNegativeReals
-    )
-
-    model.z = pyo.Var(
-        [(i, l, m) for i in model.I for (l, m) in model.LM], domain=pyo.NonNegativeReals
-    )
+    model.x = pyo.Var(model.IJK, domain=pyo.NonNegativeReals)
+    model.y = pyo.Var(model.IKL, domain=pyo.NonNegativeReals)
+    model.z = pyo.Var(model.ILM, domain=pyo.NonNegativeReals)
 
     model.OBJ = pyo.Objective(expr=model.f)
 
-    model.production = pyo.Constraint(IK_IJK.keys(), rule=production_rule)
-    model.transport = pyo.Constraint(model.I, model.L, rule=transport_rule)
-    model.demand = pyo.Constraint(model.I, model.M, rule=demand_rule)
+    model.production = pyo.Constraint(model.IK, rule=production_rule)
+    model.transport = pyo.Constraint(model.IL, rule=transport_rule)
+    model.demand = pyo.Constraint(model.IM, rule=demand_rule)
 
     # model.write("int.lp")
 
@@ -212,22 +202,16 @@ def pyomo(I, L, M, IJ, JK, IK, KL, LM, D, solve):
 
 
 def production_rule(model, i, k):
-    lhs = [model.x[i, j, k] for i, j, k in model.IK_IJK[i, k]]
-    rhs = [model.y[i, k, l] for (kk, l) in model.KL if kk == k]
-
-    if lhs and rhs:
-        return sum(lhs) >= sum(rhs)
-    else:
-        return pyo.Constraint.Skip
+    return sum(model.x[ijk] for ijk in model.IK_IJK[i, k]) >= sum(
+        model.y[ikl] for ikl in model.IK_IKL[i, k]
+    )
 
 
 def transport_rule(model, i, l):
-    if len(model.L_K[l]) or len(model.L_M[l]):
-        return sum(model.y[i, k, l] for k in model.L_K[l]) >= sum(
-            model.z[i, l, m] for m in model.L_M[l]
-        )
-    return pyo.Constraint.Skip
+    return sum(model.y[ikl] for ikl in model.IL_IKL[i, l]) >= sum(
+        model.z[ilm] for ilm in model.IL_ILM[i, l]
+    )
 
 
 def demand_rule(model, i, m):
-    return sum(model.z[i, l, m] for l in model.M_L[m]) >= model.d[i, m]
+    return sum(model.z[ilm] for ilm in model.IM_ILM[i, m]) >= model.d[i, m]
--- supply_chain/supply_chain.jl
+++ supply_chain/supply_chain.jl
@@ -22,10 +22,63 @@ function read_variable_data(n)
     ILM = read_tuple_list("supply_chain/data/data_ILM_$n.json")
     d = read_tuple_list("supply_chain/data/data_D_$n.json")
     D = Dict()
-    for x in d
-        D[(x[1], x[2])] = x[3]
+    for (i, m, value) in d
+        D[(i, m)] = value
     end
     return IK, IL, IM, IJK, IKL, ILM, D
+end
+
+function convert_to_dict(IK, IL, IM, IJK, IKL, ILM)
+    ik_ijk = Dict()
+    ik_ikl = Dict()
+    for (i, k) in IK
+        ik_ijk_list = []
+        ik_ikl_list = []
+        for (ii, j, kk) in IJK
+            if ii == i && kk == k
+                push!(ik_ijk_list, (i, j, k))
+            end
+        end
+        for (ii, kk, l) in IKL
+            if ii == i && kk == k
+                push!(ik_ikl_list, (i, k, l))
+            end
+        end
+        ik_ijk[(i, k)] = ik_ijk_list
+        ik_ikl[(i, k)] = ik_ikl_list
+    end
+
+    il_ikl = Dict()
+    il_ilm = Dict()
+    for (i, l) in IL
+        il_ikl_list = []
+        il_ilm_list = []
+        for (ii, k, ll) in IKL
+            if ii == i && ll == l
+                push!(il_ikl_list, (i, k, l))
+            end
+        end
+        for (ii, ll, m) in ILM
+            if ii == i && ll == l
+                push!(il_ilm_list, (i, l, m))
+            end
+        end
+        il_ikl[(i, l)] = il_ikl_list
+        il_ilm[(i, l)] = il_ilm_list
+    end
+
+    im_ilm = Dict()
+    for (i,m) in IM
+        im_ilm_list = []
+        for (ii,l,mm) in ILM
+            if ii==i && mm ==m
+                push!(im_ilm_list, (i,l,m))
+            end
+        end
+        im_ilm[(i,m)] = im_ilm_list
+    end
+
+    return ik_ijk, ik_ikl, il_ikl, il_ilm, im_ilm
 end
 
 function intuitive_jump(IK, IL, IM, IJK, IKL, ILM, D, solve)
@@ -58,7 +111,7 @@ function intuitive_jump(IK, IL, IM, IJK, IKL, ILM, D, solve)
     end
 end
 
-function jump(I, L, M, IJ, JK, IK, KL, LM, D, solve)
+function jump(IK, IL, IM, IJK, IKL, ILM, IK_IJK, IK_IKL, IL_IKL, IL_ILM, IM_ILM, D, solve)
     model = if solve == "True"
         direct_model(Gurobi.Optimizer())
     else
@@ -67,43 +120,27 @@ function jump(I, L, M, IJ, JK, IK, KL, LM, D, solve)
 
     set_string_names_on_creation(model, false)
 
-    x_list = [
-        (i, j, k)
-        for (i, j) in IJ
-        for (jj, k) in JK if jj == j
-        for (ii, kk) in IK if ii == i && kk == k
-    ]
-
-    y_list = [
-        (i, k, l)
-        for i in I
-        for (k, l) in KL
-    ]
-
-    z_list = [(i, l, m) for i in I for (l, m) in LM]
-
-    @variable(model, x[x_list] >= 0)
-    @variable(model, y[y_list] >= 0)
-    @variable(model, z[z_list] >= 0)
+    @variable(model, x[IJK] >= 0)
+    @variable(model, y[IKL] >= 0)
+    @variable(model, z[ILM] >= 0)
 
     for (i, k) in IK
         @constraint(model,
             sum(
-                x[(i, j, k)]
-                for (ii, j) in IJ if ii == i
-                for (jj, kk) in JK if jj == j && kk == k
-            ) >= sum(y[(i, k, l)] for (kk, l) in KL if kk == k)
-        )
+                x[ijk] for ijk in IK_IJK[(i, k)]
+            ) >= sum(y[ikl] for ikl in IK_IKL[(i, k)]))
     end
 
-    for i in I, l in L
-        @constraint(model,
-            sum(y[(i, k, l)] for (k, ll) in KL if ll == l) >=
-            sum(z[(i, l, m)] for (ll, m) in LM if ll == l))
+    for (i, l) in IL
+        @constraint(model, sum(
+            y[ikl] for ikl in IL_IKL[(i, l)]
+        ) >= sum(z[ilm] for ilm in IL_ILM[(i, l)]))
     end
 
-    for i in I, m in M
-        @constraint(model, sum(z[(i, l, m)] for (l, mm) in LM if mm == m) >= D[i, m])
+    for (i, m) in IM
+        @constraint(model, sum(
+            z[ilm] for ilm in IM_ILM[(i,m)]
+        ) >= D[i, m])
     end
 
     if solve == "True"
@@ -130,12 +167,13 @@ tt = DataFrame(I=Int[], Language=String[], MinTime=Float64[], MeanTime=Float64[]
 
 for n in N
     IK, IL, IM, IJK, IKL, ILM, D = read_variable_data(n)
+    IK_IJK, IK_IKL, IL_IKL, IL_ILM, IM_ILM = convert_to_dict(IK, IL, IM, IJK, IKL, ILM)
 
-    # if maximum(t.MinTime; init=0) < time_limit
-    #     r = @benchmark jump($I, $L, $M, $IJ, $JK, $IK, $KL, $LM, $D, $solve) samples = samples evals = evals
-    #     push!(t, (n, "JuMP", minimum(r.times) / 1e9, mean(r.times) / 1e9, median(r.times) / 1e9))
-    #     println("JuMP done $n in $(round(minimum(r.times) / 1e9, digits=2))s")
-    # end
+    if maximum(t.MinTime; init=0) < time_limit
+        r = @benchmark jump($IK, $IL, $IM, $IJK, $IKL, $ILM, $IK_IJK, $IK_IKL, $IL_IKL, $IL_ILM, $IM_ILM, $D, $solve) samples = samples evals = evals
+        push!(t, (n, "JuMP", minimum(r.times) / 1e9, mean(r.times) / 1e9, median(r.times) / 1e9))
+        println("JuMP done $n in $(round(minimum(r.times) / 1e9, digits=2))s")
+    end
 
     if maximum(tt.MinTime; init=0) < time_limit
         rr = @benchmark intuitive_jump($IK, $IL, $IM, $IJK, $IKL, $ILM, $D, $solve) samples = samples evals = evals
